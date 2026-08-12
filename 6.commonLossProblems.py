e6d6bd3cb58e92e487ac92eb5c2ee8d63039bd86
Em <murriumcra09@gmail.com>
2026-08-11 21:26:54 -0400

    Add files via upload

---
+++ 6.commonLossProblems.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+# Common Loss Functions page 50
+print(f"\nCommon Loss Functions\n")
+
+# Example: Regression Loss
+criterion_mse = nn.MSELoss()
+
+prediction = torch.tensor([10.0]) # Model predicts 10
+target = torch.tensor([15.0]) # Truth is 15
+
+loss = criterion_mse(prediction, target)
+print(f"MSE Loss: {loss.item()}") # (15-10)^2 = 25
