3d852525ba9448bbeb66c77724a961ff4d6fd373
Em <murriumcra09@gmail.com>
2026-08-12 19:50:19 -0400

    Add learning rate example with Adam optimizer

---
+++ 6.theLearningRate.py
@@ -0,0 +1,9 @@
+import torch.optim as optim
+
+print(f"\nThe Learning Rate\n")
+# Assuming 'model' is a neural network we created
+model = nn.Linear(10, 1)
+print(f"model = {model}")
+
+# Create an optimizer (Adam) and give it the model's parameters
+optimizer = optim.Adam(model.parameters(), lr = 0.001)
