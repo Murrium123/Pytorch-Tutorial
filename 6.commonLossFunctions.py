fd6ec03f6b69b1c7f04ed87b2e6031ad5404afe2
Em <murriumcra09@gmail.com>
2026-08-11 21:33:49 -0400

    Fix MSE Loss calculation in commonLossFunctions.py

--- 6.commonLossFunctions.py
+++ 6.commonLossFunctions.py
@@ -11,4 +11,4 @@ prediction = torch.tensor([10.0]) # Model predicts 10
 target = torch.tensor([14.0]) # Truth is 14
 
 loss = criterion_mse(prediction, target)
-print(f"MSE Loss: {loss.item()}") # (15-10)^2 = 25
+print(f"MSE Loss: {loss.item()}") # (14-10)^2 = 16
